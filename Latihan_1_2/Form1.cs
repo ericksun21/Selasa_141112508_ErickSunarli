0d37252f071120761ce441a6405264a19935f63e
ericksun21 <ericksunarli@yahoo.com>
2016-10-12 18:02:43 +0700

    commit pertama

---
+++ Latihan_1_2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan_1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DTP_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+            label2.Text = vScrollBar2.Value.ToString();
+            DTP.MinDate = DateTime.Today.AddYears(-vScrollBar2.Value);
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            label1.Text = vScrollBar1.Value.ToString();
+            DTP.MaxDate = DateTime.Today.AddYears(vScrollBar1.Value);
+        }
+    }
+}
