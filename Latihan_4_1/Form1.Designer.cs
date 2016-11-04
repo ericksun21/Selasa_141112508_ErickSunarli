ba67f65e291608d85db195e700805576f6a8d34d
ericksun21 <ericksunarli@yahoo.com>
2016-11-03 21:53:14 -0700

    commit keempat

---
+++ Latihan_4_1/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Latihan_4_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.typeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Color1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelFilename = new System.Windows.Forms.Label();
+            this.textboxArea = new System.Windows.Forms.RichTextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(669, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.newToolStripMenuItem,
+            this.buttonSave,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click_1);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click_1);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.buttonSave.Size = new System.Drawing.Size(152, 22);
+            this.buttonSave.Text = "Save";
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click_1);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click_1);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.typeToolStripMenuItem,
+            this.Color1,
+            this.backgroundColorToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.helpToolStripMenuItem.Text = "Font";
+            // 
+            // typeToolStripMenuItem
+            // 
+            this.typeToolStripMenuItem.Name = "typeToolStripMenuItem";
+            this.typeToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.typeToolStripMenuItem.Text = "Type";
+            this.typeToolStripMenuItem.Click += new System.EventHandler(this.typeToolStripMenuItem_Click_1);
+            // 
+            // Color1
+            // 
+            this.Color1.Name = "Color1";
+            this.Color1.Size = new System.Drawing.Size(168, 22);
+            this.Color1.Text = "Color";
+            this.Color1.Click += new System.EventHandler(this.Color1_Click);
+            // 
+            // backgroundColorToolStripMenuItem
+            // 
+            this.backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
+            this.backgroundColorToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.backgroundColorToolStripMenuItem.Text = "Background color";
+            this.backgroundColorToolStripMenuItem.Click += new System.EventHandler(this.backgroundColorToolStripMenuItem_Click_1);
+            // 
+            // labelFilename
+            // 
+            this.labelFilename.AutoSize = true;
+            this.labelFilename.Location = new System.Drawing.Point(25, 275);
+            this.labelFilename.Name = "labelFilename";
+            this.labelFilename.Size = new System.Drawing.Size(49, 13);
+            this.labelFilename.TabIndex = 5;
+            this.labelFilename.Text = "Filename";
+            // 
+            // textboxArea
+            // 
+            this.textboxArea.Location = new System.Drawing.Point(12, 32);
+            this.textboxArea.Name = "textboxArea";
+            this.textboxArea.Size = new System.Drawing.Size(645, 237);
+            this.textboxArea.TabIndex = 4;
+            this.textboxArea.Text = "";
+            this.textboxArea.TextChanged += new System.EventHandler(this.textboxArea_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 292);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.labelFilename);
+            this.Controls.Add(this.textboxArea);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buttonSave;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem typeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Color1;
+        private System.Windows.Forms.ToolStripMenuItem backgroundColorToolStripMenuItem;
+        private System.Windows.Forms.Label labelFilename;
+        private System.Windows.Forms.RichTextBox textboxArea;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Latihan_4_1/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Latihan_4_1
+{
+    public partial class Form1 : Form
+    {
+        string filename;
+        Boolean newfile = false;
+        //
+        public Form1()
+        {
+            InitializeComponent();
+            textboxArea.Enabled = false;
+            buttonSave.Enabled = false;
+            labelFilename.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog window_open_dialog = new OpenFileDialog();
+
+            if (window_open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader stream_read = new StreamReader(File.OpenRead(window_open_dialog.FileName));
+                textboxArea.Text = stream_read.ReadToEnd();
+                stream_read.Dispose();
+                //
+                textboxArea.Enabled = true;
+                buttonSave.Enabled = true;
+                filename = window_open_dialog.FileName;
+                labelFilename.Text = filename;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            StreamWriter stream_write = new StreamWriter(filename);
+            stream_write.Write(textboxArea.Text);
+            stream_write.Dispose();
+            //
+            labelFilename.Text = filename;
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newfile = true;
+            textboxArea.Enabled = true;
+            buttonSave.Enabled = true;
+            textboxArea.Text = "";
+            labelFilename.Text = "Filename : *untitled";
+        }
+
+        private void textboxArea_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult warna = colorDialog1.ShowDialog();
+            if (warna == DialogResult.OK)
+            {
+                textboxArea.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        private void typeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+            fd.Font = textboxArea.SelectionFont;
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                textboxArea.SelectionFont = fd.Font;
+            }
+        }
+
+        private void backgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.Color = textboxArea.BackColor;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textboxArea.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textboxArea_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            newfile = true;
+            textboxArea.Enabled = true;
+            buttonSave.Enabled = true;
+            textboxArea.Text = "";
+            labelFilename.Text = "Filename : *untitled";
+        }
+
+        private void openToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog window_open_dialog = new OpenFileDialog();
+
+            if (window_open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader stream_read = new StreamReader(File.OpenRead(window_open_dialog.FileName));
+                textboxArea.Text = stream_read.ReadToEnd();
+                stream_read.Dispose();
+                //
+                textboxArea.Enabled = true;
+                buttonSave.Enabled = true;
+                filename = window_open_dialog.FileName;
+                labelFilename.Text = filename;
+            }
+        }
+
+        private void buttonSave_Click_1(object sender, EventArgs e)
+        {
+            StreamWriter stream_write = new StreamWriter(filename);
+            stream_write.Write(textboxArea.Text);
+            stream_write.Dispose();
+            //
+            labelFilename.Text = filename;
+        }
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void backgroundColorToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            colorDialog1.Color = textboxArea.BackColor;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textboxArea.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void typeToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+            fd.Font = textboxArea.SelectionFont;
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                textboxArea.SelectionFont = fd.Font;
+            }
+        }
+
+        private void Color1_Click(object sender, EventArgs e)
+        {
+            DialogResult warna = colorDialog1.ShowDialog();
+            if (warna == DialogResult.OK)
+            {
+                textboxArea.ForeColor = colorDialog1.Color;
+            }
+        }
+    }
+}
