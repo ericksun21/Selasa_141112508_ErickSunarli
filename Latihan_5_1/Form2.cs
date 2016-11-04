f7f648109eaa782f70ca0a709e4c5f73e3b1662f
ericksun21 <ericksunarli@yahoo.com>
2016-11-03 22:36:22 -0700

    commit kelima

---
+++ Latihan_5_1/Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan_5_1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            TreeNode ParentNode1;
+
+            ParentNode1 = treeView1.Nodes.Add("Color");
+            ParentNode1.Nodes.Add("Background Color");
+            ParentNode1.Nodes.Add("Font Color");
+            ParentNode1.Collapse();
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect_1(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void treeView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            TreeNode node = treeView1.SelectedNode;
+            colorDialog1.Color = treeView1.BackColor;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                treeView1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+
+        }
+
+    }
+}
